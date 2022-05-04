8dff55481dc73ec9b016df59885ae48e1cc1c62f
Wendel Demétrio <hey.wendel95@gmail.com>
2022-05-04 08:49:02 -0300

    stage02 completed!

--- src/components/WidgetForm/Steps/FeedbackContent.tsx
+++ src/components/WidgetForm/Steps/FeedbackContent.tsx
@@ -1,23 +1,39 @@
-import { ArrowLeft } from 'phosphor-react';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
+
 import { FeedbackType, feedbackTypes } from '..';
 import { CloseButton } from '../../CloseButton';
 import { ScreenshotButton } from '../ScreenshotButton';
+import { ArrowLeft } from 'phosphor-react';
 
 interface FeedbackContentProps {
   feedbackType: FeedbackType;
   onBackButtonPress: () => void;
+  onFeedbackSent: () => void;
 }
 
 export const FeedbackContent = ({
   feedbackType,
   onBackButtonPress,
+  onFeedbackSent,
 }: FeedbackContentProps) => {
   const [screenshot, setScreenshot] = useState<
     string | null
   >(null);
+  const [comment, setComment] = useState('');
 
   const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  function handleSubmitFeedback(event: FormEvent) {
+    event.preventDefault();
+
+    console.log({
+      comment,
+      screenshot,
+    });
+
+    onFeedbackSent();
+  }
+
   return (
     <>
       <header>
@@ -39,10 +55,14 @@ export const FeedbackContent = ({
         </span>
       </header>
 
-      <form className="my-4 max-w-full">
+      <form
+        onSubmit={handleSubmitFeedback}
+        className="my-4 max-w-full"
+      >
         <textarea
           className="min-w-[384px] w-full min-h-[112px] text-sm placeholder-zinc-400 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:outline-none focus:ring-1 resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
           placeholder="Conte os detalhes do que está acontecendo..."
+          onChange={(evt) => setComment(evt.target.value)}
         />
         <footer className="flex gap-2 mt-2">
           <ScreenshotButton
@@ -51,8 +71,9 @@ export const FeedbackContent = ({
           />
 
           <button
+            disabled={comment.length === 0}
             type="submit"
-            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
           >
             Enviar feedback
           </button>
--- src/components/WidgetForm/Steps/FeedbackSuccess.tsx
+++ src/components/WidgetForm/Steps/FeedbackSuccess.tsx
@@ -1,1 +1,36 @@
-export const FeedbackSuccess = () => {};
+import { CloseButton } from '../../CloseButton';
+
+import successImageUrl from '../../../assets/success.svg';
+
+interface FeedbackSuccessProps {
+  onFeedbackSendAnother: () => void;
+}
+
+export const FeedbackSuccess = ({
+  onFeedbackSendAnother,
+}: FeedbackSuccessProps) => {
+  return (
+    <>
+      <header>
+        <CloseButton />
+      </header>
+      <div className="flex flex-col items-center py-10 w-[304px]">
+        <img
+          src={successImageUrl}
+          alt="Green icon with white v to represent success"
+          className="w-10 h-10"
+        />
+        <span className="text-xl mt-2">
+          Agradecemos o feedback!
+        </span>
+        <button
+          type="button"
+          className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 focus:border-brand-500 focus:ring-brand-500 focus:outline-none focus:ring-1 transition-colors"
+          onClick={onFeedbackSendAnother}
+        >
+          Quero enviar outro
+        </button>
+      </div>
+    </>
+  );
+};
